b7f814f323d408dfdea1fc69bb87ef7217fafc30
GeovaniProgram <geovani.programador.hatuu@gmail.com>
2024-05-05 14:53:01 -0300

    Iniciando o git

---
+++ myscript.js
@@ -0,0 +1,30 @@
+//armazenando na máquina do usuario o tema
+localStorage.setItem('theme', 'dark'); 
+
+//acessado o tema da maquina do usuário
+localStorage.getItem('theme'); 
+// dark
+
+function atualizarRelogio() {
+    var data = new Date();
+    var horas = data.getHours();
+    var minutos = data.getMinutes();
+    var segundos = data.getSeconds();
+
+    horas = formatarTempo(horas);
+    minutos = formatarTempo(minutos);
+    segundos = formatarTempo(segundos);
+
+    document.getElementById("relogio").innerText = horas + ":" + minutos + ":" + segundos;
+}
+
+function formatarTempo(tempo) {
+    return tempo < 10 ? "0" + tempo : tempo;
+}
+
+setInterval(atualizarRelogio, 1000);
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
