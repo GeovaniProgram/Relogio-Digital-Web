171ec1e9f6aa816744bc4d547190ec35e09efd2e
GeovaniProgram <geovani.programador.hatuu@gmail.com>
2024-05-09 19:33:38 -0300

    Adicionando novas imagens para hiper texto e modificação das tags e adição do footer

--- myscript.js
+++ myscript.js
@@ -1,9 +1,4 @@
-//armazenando na máquina do usuario o tema
-localStorage.setItem('theme', 'dark'); 
 
-//acessado o tema da maquina do usuário
-localStorage.getItem('theme'); 
-// dark
 
 function atualizarRelogio() {
     var data = new Date();
