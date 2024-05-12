ec5a7ce539b1ca1ac629cad97e3460e769f2c14e
GeovaniProgram <geovani.programador.hatuu@gmail.com>
2024-05-12 16:25:01 -0300

    Adicionado mudança de img do botão dark ao clique

--- myscript.js
+++ myscript.js
@@ -18,8 +18,15 @@ function formatarTempo(tempo) {
 }
 
 setInterval(atualizarRelogio, 1000);
+var dark = 0;
 
 function darkMode() {
     var element = document.body;
+    dark = dark + 1;
     element.classList.toggle("dark-mode");
+    document.getElementById("botaodark").src = "/assets/light-mode.png";
+    if (dark == 2){
+        document.getElementById("botaodark").src = "/assets/dark-mode.png";
+        dark = 0
+    }
 }
