b02f878783d220984ab6fdf0773fbdd40a8e9f89
GeovaniProgram <geovani.programador.hatuu@gmail.com>
2024-05-12 16:52:10 -0300

    Configurando para todas as imagens mudarem com o tema

--- myscript.js
+++ myscript.js
@@ -25,7 +25,11 @@ function darkMode() {
     dark = dark + 1;
     element.classList.toggle("dark-mode");
     document.getElementById("botaodark").src = "/assets/light-mode.png";
+    document.getElementById("github").src = "/assets/github-icon/github-icon-lightmode.png";
+    document.getElementById("linkedin").src ="/assets/linkedin-icon/linkidin-icon-lightmode.png";
     if (dark == 2){
+        document.getElementById("github").src = "/assets/github-icon/github-icon-darkmode.png";
+        document.getElementById("linkedin").src ="/assets/linkedin-icon/linkidin-icon-darkmode.png";
         document.getElementById("botaodark").src = "/assets/dark-mode.png";
         dark = 0
     }
